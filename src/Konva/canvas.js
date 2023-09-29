3c7e2b7f94728f42a5206d3eec4ad28519b913e6
Harshavardhan-Pandurangan <96712770+Harshavardhan-Pandurangan@users.noreply.github.com>
2023-09-29 09:40:57 +0530

    code structure commit

---
+++ src/Konva/canvas.js
@@ -0,0 +1,4 @@
+import React, { useState } from "react";
+import { Stage, Layer, Rect } from "react-konva";
+
+export default function Canvas() {}
--- src/Konva/konva.js
+++ src/Konva/konva.js
@@ -1,1 +1,22 @@
-export default function Konva() {}
+import Canvas from "./canvas";
+import Controls from "./controls";
+
+export default function Konva() {
+    return (
+        <div class="flex flex-col h-screen">
+            <div>
+                <p class="font-mono text-6xl font-extrabold text-center mt-7">
+                    Annotation with Konva
+                </p>
+            </div>
+            <div class="flex flex-grow mt-7">
+                <div class="w-2/3 h-full">
+                    <Canvas />
+                </div>
+                <div class="w-1/3 h-full">
+                    <Controls />
+                </div>
+            </div>
+        </div>
+    );
+}
